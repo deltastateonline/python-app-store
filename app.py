db4261783bb7f116363ed0c9fec360a8d4be6449
joomla@deltastateonline.com <joomla@deltastateonline.com>
2023-03-27 08:16:45 +1000

    Using access token and refresh tokens

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
-import secrets
-import uuid
 from flask import Flask,jsonify
 from flask_smorest import Api
 
 from db import  db
-import models
+from blocklist import BLOCKLIST
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
@@ -31,6 +29,26 @@ def create_app(db_url=None):
 
     jwt = JWTManager(app)
 
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    #message to return
+    @jwt.revoked_token_loader
+    def revoke_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"description": "Token has been revoked", "error":"token_revoked"})
+            , 401
+        )
+
+    @jwt.additional_claims_loader
+    def add_claim_to_jwt(identity):
+
+        if identity["user_id"] == 1:
+            return {"is_admin": True}
+        else:
+            return {"is_admin": False}
+
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (
@@ -43,11 +61,21 @@ def create_app(db_url=None):
             jsonify({"description": "Invalid access token.",
                      "error": "authorization_required"}), 401
         )
+
     @jwt.unauthorized_loader
     def missing_token_callback(error):
         return (
             jsonify({"description":"Request does not contain an access token.",
                      "error": "authorization_required"}), 401
+        )
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_h, jwt_p):
+        return(
+            jsonify({
+                "description": "The token is not fresh",
+                "error": "fresh_token_required"
+            }), 401
         )
 
     with app.app_context():
--- resources/user.py
+++ resources/user.py
@@ -1,11 +1,12 @@
 import uuid
-from flask import Flask, request
+
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required,get_jwt_identity, get_jwt
 from sqlalchemy.exc import SQLAlchemyError
 from db import db
+from blocklist import BLOCKLIST
 
 from models import UserModel
 from schemas import UserSchema
@@ -50,7 +51,25 @@ class UserLogin(MethodView):
         ).first()
 
         if user and pbkdf2_sha256.verify(user_data["password"], user.password):
-            access_token = create_access_token(identity={"user_id":user.id, "username":user.username})
-            return {"access_token": access_token}
+            access_token = create_access_token(identity={"user_id":user.id, "username":user.username},
+                                               fresh=True)
+            refresh_token = create_refresh_token(identity={"user_id":user.id, "username":user.username})
+            return {"access_token": access_token, "refresh_token":refresh_token}
 
         abort(401, message="Invalid Credentials.")
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token= create_access_token(identity=current_user,fresh=False)
+        return {"access_token": new_token}
+@blp.route("/logout")
+class UserLogout(MethodView):
+
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully Logged Out."}
